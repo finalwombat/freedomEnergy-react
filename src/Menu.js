25937508a968a3cc3b51ac0256d252b59d07877a
Jay Kelly <comital333@gmail.com>
2017-05-22 18:56:43 +1000

    added new components and added to menu

--- src/Menu.js
+++ src/Menu.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react'
 import { Menu as SemanticMenu} from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Home from './Home'
-import About from './About'
-import Products from './Products'
+import Residential from './Residential'
+import Commercial from './Commercial'
+import Utilities from './Utilities'
+import Contact from './Contact'
+
 
 export default class Menu extends Component {
   state = { activeItem: 'home' }
@@ -17,13 +20,17 @@ export default class Menu extends Component {
         <div>
           <SemanticMenu pointing secondary>
             <Link to="/"><SemanticMenu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/></Link>
-            <Link to="/about"><SemanticMenu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick}/></Link>
-            <Link to="/products"><SemanticMenu.Item name='products' active={activeItem === 'products'} onClick={this.handleItemClick}/></Link>
+            <Link to="/residential"><SemanticMenu.Item name='residential' active={activeItem === 'residential'} onClick={this.handleItemClick}/></Link>
+            <Link to="/commercial"><SemanticMenu.Item name='commercial' active={activeItem === 'commercial'} onClick={this.handleItemClick}/></Link>
+            <Link to="/utilities"><SemanticMenu.Item name='utilities' active={activeItem === 'utilities'} onClick={this.handleItemClick}/></Link>
+            <Link to="/contact"><SemanticMenu.Item name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/></Link>
           </SemanticMenu>
           <div>
           <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/Products" component={Products}/>
+          <Route path="/residential" component={Residential}/>
+          <Route path="/commercial" component={Commercial}/>
+          <Route path="/utilities" component={Utilities}/>
+          <Route path="/Contact" component={Contact}/>
           </div>
         </div>
       </Router>
