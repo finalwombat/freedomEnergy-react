9fa80e9c63b2334b910eb6556fa924ea9e3e23ee
Jay Kelly <comital333@gmail.com>
2017-06-01 12:37:05 +1000

    removed unneeded className

--- src/imageSlider/Slide.js
+++ src/imageSlider/Slide.js
@@ -8,7 +8,10 @@ const Slide = (props) => {
     backgroundSize: 'cover',
     backgroundPosition: 'center'
   }
-  return <div style={background} className="slide slide-in"></div>
+  return (
+    <div style={background} className="slide">
+    </div>
+  )
 }
 
 export default Slide
