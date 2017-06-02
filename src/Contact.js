5e30e79a57baea8ad74d075928e1470cfe99dc60
Jay Kelly <comital333@gmail.com>
2017-06-02 13:52:55 +1000

    added form to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Button, Form, input, Message} from 'semantic-ui-react'
 
-const Contact = () => {
-  return (
-    <div><h2>Contact</h2></div>
-  )
+class Contact extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Contact</h2>
+        <Form success>
+          <Form.Field>
+            <label>First Name</label>
+            <input placeholder='First Name'></input>
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input placeholder='Last Name'></input>
+          </Form.Field>
+          <Form.Input label='Email' placeholder='joe@schmoe.com' />
+          <Message
+            success
+            header='Success'
+            content="We will contact you shortly"
+          />
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+
+    )
+  }
 
 }
 
