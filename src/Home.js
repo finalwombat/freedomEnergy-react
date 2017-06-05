bc91c65576145683ee195ce6a4d6341a7c1cdbe1
Jay Kelly <comital333@gmail.com>
2017-06-06 09:49:11 +1000

    add more cards

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Slider from './imageSlider/Slider'
+import { Card, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
+    <div className="homeContainer">
       <Slider />
+      <div className="mainContent">
+        <div className="cards">
+          <Card>
+            <Image src='../public/images/commerce1.jpg' />
+            <Card.Content>
+              <Card.Header><Link to='/residential'>Residential</Link></Card.Header>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='../public/images/commerce1.jpg' />
+            <Card.Content>
+              <Card.Header><Link to='/commercial'>Commercial</Link></Card.Header>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='../public/images/commerce1.jpg' />
+            <Card.Content>
+              <Card.Header><Link to='/utilities'>Utilities</Link></Card.Header>
+            </Card.Content>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 
