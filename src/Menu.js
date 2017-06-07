3915138121659cc4263194931d60c5a536df8914
Jay Kelly <comital333@gmail.com>
2017-06-07 10:06:58 +1000

    change menu to use App level location state

--- src/Menu.js
+++ src/Menu.js
@@ -9,25 +9,33 @@ import Contact from './Contact'
 
 
 export default class Menu extends Component {
-  state = { activeItem: 'home' }
+  constructor(props){
+    super(props)
+    this.state = { activeItem: this.props.location }
+    console.log(this.props)
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name})
+  componentWillReceiveProps(nextProps){
+    console.log(nextProps)
+    this.setState({activeItem: nextProps.location})
+  }
+
+
+  handleItemClick = (e, { name }) => {
+  this.props.handleItemClick(name ? name : '/')
+  }
 
   render(){
     const {activeItem} = this.state
     return (
 
         <div className="mainMenu">
-          <Link to="/"><img src="logo.png" className="logo" onClick={this.handleItemClick}/></Link>
+          <Link to="/" name='/'><img src="logo.png" className="logo"  onClick={this.handleItemClick}/></Link>
           <SemanticMenu secondary pointing >
-            <Link to="/residential">
-              <SemanticMenu.Item name='residential'
-                active={activeItem === 'residential'}
-                onClick={this.handleItemClick}/>
-            </Link>
-            <Link to="/commercial"><SemanticMenu.Item name='commercial' active={activeItem === 'commercial'} onClick={this.handleItemClick}/></Link>
-            <Link to="/utilities"><SemanticMenu.Item name='utilities' active={activeItem === 'utilities'} onClick={this.handleItemClick}/></Link>
-            <Link to="/contact"><SemanticMenu.Item name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick}/></Link>
+            <Link to="/residential"><SemanticMenu.Item name='/residential'active={activeItem === '/residential'} onClick={this.handleItemClick}/></Link>
+            <Link to="/commercial"><SemanticMenu.Item name='/commercial' active={activeItem === '/commercial'} onClick={this.handleItemClick}/></Link>
+            <Link to="/utilities"><SemanticMenu.Item name='/utilities' active={activeItem === '/utilities'} onClick={this.handleItemClick}/></Link>
+            <Link to="/contact"><SemanticMenu.Item name='/contact' active={activeItem === '/contact'} onClick={this.handleItemClick}/></Link>
           </SemanticMenu>
         </div>
 
