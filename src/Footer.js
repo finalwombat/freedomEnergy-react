c2389976e1ea30132283a07597b661e31695e270
Jay Kelly <comital333@gmail.com>
2017-06-07 11:04:18 +1000

    change facebook button colour

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer'>
         <Link to="/"><h1>Freedom Solar</h1></Link>
         <div className='links'>
-          <Button color='white' icon='facebook' />
+          <Button color='blue' icon='facebook' />
           <Link to="/residential"><h3>Residential</h3></Link>
           <Link to="/commercial"><h3>Commercial</h3></Link>
           <Link to="/utility"><h3>utility</h3></Link>
