092fd39b2d496d3f45bd7e94cb58dcaa5b2a2ac0
Jay Kelly <comital333@gmail.com>
2017-06-07 11:07:20 +1000

    remove segment import

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Segment } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
