0493d2bd2b56bce2000b9da8e8dabb64a3d578d0
Jay Kelly <comital333@gmail.com>
2017-06-07 10:06:24 +1000

    added location state and callback

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import Menu from './Menu'
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 import Routes from './Routes'
 import Footer from './Footer'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {location: '/'}
+    this.getLocation = this.getLocation.bind(this)
+    this.setLocation = this.setLocation.bind(this)
+  }
+
+
+  setLocation(name){
+    const location = name ? name : this.getLocation()
+    this.setState({location})
+    console.log(this.state)
+  }
+
+  componentWillMount(){
+    this.setLocation()
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
-          <Menu />
-          <Routes />
-          <Footer />
+          <Menu handleItemClick={this.setLocation} location={this.state.location}/>
+          <Routes setLocation={this.setLocation}/>
+          <Footer setLocation={this.setLocation}/>
         </div>
       </Router>
     );
   }
+
+  getLocation() {
+    const loc = {location}
+    return(loc.location.pathname)
+  }
 }
+
+
 
 export default App;
