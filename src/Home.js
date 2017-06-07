4cfd408287f856e339e5f3ebac623b6195d2fe57
Jay Kelly <comital333@gmail.com>
2017-06-07 10:44:59 +1000

    pass handleItemClick prop to components

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,13 @@ import Slider from './imageSlider/Slider'
 import { Card, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
-const Home = () => {
+const Home = (props) => {
+
+  console.log(props)
+  const handleItemClick = (e, { name }) => {
+  props.handleItemClick(name)
+  }
+
   return (
     <div className="homeContainer">
       <Slider />
@@ -12,19 +18,19 @@ const Home = () => {
           <Card>
             <Image src='../public/images/commerce1.jpg' />
             <Card.Content>
-              <Card.Header><Link to='/residential'>Residential</Link></Card.Header>
+              <Card.Header onClick={handleItemClick} name='/residential'><Link to='/residential'>Residential</Link></Card.Header>
             </Card.Content>
           </Card>
           <Card>
             <Image src='../public/images/commerce1.jpg' />
             <Card.Content>
-              <Card.Header><Link to='/commercial'>Commercial</Link></Card.Header>
+              <Card.Header name='/commercial'><Link to='/commercial'>Commercial</Link></Card.Header>
             </Card.Content>
           </Card>
           <Card>
             <Image src='../public/images/commerce1.jpg' />
             <Card.Content>
-              <Card.Header><Link to='/utilities'>Utilities</Link></Card.Header>
+              <Card.Header name='/utilities'><Link to='/utilities'>Utilities</Link></Card.Header>
             </Card.Content>
           </Card>
         </div>
