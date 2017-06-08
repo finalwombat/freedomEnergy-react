e7b74a9d97624c7988041495c021cbbd70d8153f
Jay Kelly <comital333@gmail.com>
2017-06-09 09:56:57 +1000

    import images

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Slider from './imageSlider/Slider'
 import { Card, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import residentialImage from '../public/images/commerce1.jpg'
+import commercialImage from '../public/images/commerce1.jpg'
+import utilityImage from '../public/images/commerce1.jpg'
 
 const Home = (props) => {
 
@@ -15,19 +18,19 @@ const Home = (props) => {
       <div className="mainContent">
         <div className="cards">
           <Card>
-            <Image src='../public/images/commerce1.jpg' />
+            <Image src={residentialImage} />
             <Card.Content>
               <Card.Header onClick={handleItemClick} name='/residential'><Link to='/residential'>Residential</Link></Card.Header>
             </Card.Content>
           </Card>
           <Card>
-            <Image src='../public/images/commerce1.jpg' />
+            <Image src={commercialImage} />
             <Card.Content>
               <Card.Header onClick={handleItemClick} name='/commercial'><Link to='/commercial'>Commercial</Link></Card.Header>
             </Card.Content>
           </Card>
           <Card>
-            <Image src='../public/images/commerce1.jpg' />
+            <Image src={utilityImage}/>
             <Card.Content>
               <Card.Header onClick={handleItemClick} name='/utilities'><Link to='/utilities'>Utilities</Link></Card.Header>
             </Card.Content>
